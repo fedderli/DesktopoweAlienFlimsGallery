7a3ad27ffb415f8b6757186146cb8a7bb3760edc
fedderli <pokrzywnickikarol@gmail.com>
2025-11-18 14:52:09 +0100

    Created Model for characters and reading data from CharactersData.txt

---
+++ AlienFilms/Models/CharacterModel.cs
@@ -0,0 +1,15 @@
+﻿namespace AlienFilms.Models;
+
+public class CharacterModel
+{
+    public string? CharacterName { get; set; } = "";
+    public string? Films { get; set; } = "";
+    public string? Role  { get; set; } = "";
+    public string? Actor { get; set; } = "";
+    public string? Race { get; set; } = "";
+    public int BirthYear { get; set; } = 0;
+    public string? CrewFunction { get; set; } = "";
+    public string? Characteristic { get; set; } = "";
+    public string? Fate { get; set; } = "";
+    public string? ChatacterFunFact { get; set; } = "";
+}
